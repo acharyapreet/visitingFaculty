7f71ab53cb8592cc6631f175f06a103a4c3c6c3c
Preet Acharya <acharyapreet2@gmail.com>
2026-08-03 19:22:33 +0530

    ADDED ENV IN FRONTEND

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+require('dotenv').config();
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', 
+        target: process.env.BACKEND_URL, 
         changeOrigin: true,
       }
     }
